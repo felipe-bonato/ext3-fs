d8dd2d340884c720c53c4e1d98ea8af32cb17a90
Felipe Bonato <felipe13.bonato@gmail.com>
2022-05-08 11:52:08 -0300

    Added first part of initFs

--- fs.cpp
+++ fs.cpp
@@ -1,20 +1,38 @@
-#ifndef fs_h
-#define fs_h
-#include <string>
+#include "fs.h"
 
-typedef struct {
-    unsigned char IS_USED;             // 0x01 se utilizado, 0x00 se livre
-    unsigned char IS_DIR;              // 0x01 se diretorio, 0x00 se arquivo
-    char NAME[10];                     // nome do arquivo/dir
-    char SIZE;                         // tamanho do arquivo/dir em bytes
-    unsigned char DIRECT_BLOCKS[3];    
-    unsigned char INDIRECT_BLOCKS[3];
-    unsigned char DOUBLE_INDIRECT_BLOCKS[3];
-} INODE;
+#include <string>
+#include <fstream>
+
+typedef char c8;
+
+void _writeMetadata(std::string fsFileName, int blockSize, int numBlocks, int numInodes) {
+	std::ofstream(fsFileName, std::ios::binary) << blockSize << numBlocks << numInodes;
+	//file.write((c8*)&blockSize, sizeof(c8));
+	//file.write((c8*)&numBlocks, sizeof(c8));
+	//file.write((c8*)&numInodes, sizeof(c8));
+	
+}
+
+void _writeBitMap(std::string fsFileName, int numBlocks, int numInodes) {
+
+}
+
+void _writeInodes(std::string fsFileName, int numInodes) {
+
+}
+
+void _writeRoot(std::string fsFileName, int numInodes) {
+
+}
+
+void _writeBlocks(std::string fsFileName, int numBlocks) {
+
+}
 
 void initFs(std::string fsFileName, int blockSize, int numBlocks, int numInodes)
-{
-
+{ 
+	_writeMetadata(fsFileName, blockSize, numBlocks, numInodes);
+	
 }
 
 void addFile(std::string fsFileName, std::string filePath, std::string fileContent)
@@ -36,8 +54,3 @@ void move(std::string fsFileName, std::string oldPath, std::string newPath)
 {
 	
 }
-
-
-#endif /* fs_h */
-
-#include "fs.h"

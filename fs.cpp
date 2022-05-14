1bd04451c26e7a4d25258b3c58c63ea1cecdd16d
Felipe Bonato <felipe13.bonato@gmail.com>
2022-05-14 11:29:02 -0300

    Fixed bug on metadata only appending; Started writing bitmap

--- fs.cpp
+++ fs.cpp
@@ -3,18 +3,45 @@
 #include <string>
 #include <fstream>
 
+#include <cmath>
+
+#include <bitset>
+
 typedef char c8;
+typedef uint8_t u8;
+
+struct fileInPos
+{
+	std::ofstream m_File;
+	fileInPos(std::string& fileName, size_t position)
+	{
+		m_File = std::ofstream{fileName, std::ios::binary};
+		m_File.seekp(position);
+	}
+	~fileInPos()
+	{
+		m_File.close();
+	}
+};
 
 void _writeMetadata(std::string fsFileName, int blockSize, int numBlocks, int numInodes) {
-	std::ofstream(fsFileName, std::ios::binary) << blockSize << numBlocks << numInodes;
-	//file.write((c8*)&blockSize, sizeof(c8));
-	//file.write((c8*)&numBlocks, sizeof(c8));
-	//file.write((c8*)&numInodes, sizeof(c8));
-	
+	fileInPos file = fileInPos{fsFileName, 0};
+	std::ofstream(fsFileName, std::ios::app)
+		<< static_cast<u8>(blockSize)
+		<< static_cast<u8>(numBlocks)
+		<< static_cast<u8>(numInodes);
 }
 
-void _writeBitMap(std::string fsFileName, int numBlocks, int numInodes) {
+void _writeBitMap(std::string fsFileName, int numBlocks) {
+	std::ofstream file(fsFileName, std::ios::binary);
+	file.seekp(3); // Position where bitmap starts
 
+	size_t numBitMapBytes = std::ceil(static_cast<double>(numBlocks) / 8.0); // This is rounded up to the nearest byte
+	for (size_t byteIndex = 0; byteIndex < numBitMapBytes; byteIndex++)
+	{		
+		file << 0x00;
+	}
+	file.close();
 }
 
 void _writeInodes(std::string fsFileName, int numInodes) {
@@ -30,8 +57,14 @@ void _writeBlocks(std::string fsFileName, int numBlocks) {
 }
 
 void initFs(std::string fsFileName, int blockSize, int numBlocks, int numInodes)
-{ 
+{
+	// Resetting file
+	std::ofstream file{fsFileName, std::ios::out};
+	file.close(); // We open it and close right after cause cpp
+
+	// Acctually initiating the fs
 	_writeMetadata(fsFileName, blockSize, numBlocks, numInodes);
+	_writeBitMap(fsFileName, numBlocks);
 	
 }
 

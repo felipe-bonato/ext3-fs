db2f65a92050a33853059e8b964d6b288cd76c65
Felipe Bonato <felipe13.bonato@gmail.com>
2022-05-21 15:42:41 -0300

    Worked on add file code

--- fs.cpp
+++ fs.cpp
@@ -1,3 +1,8 @@
+/**
+ * TODO:
+ * - Refactor for fetch and get functions accept only file metadata as argument
+ */
+
 #include "fs.h"
 
 #include <vector>
@@ -11,6 +16,7 @@ typedef uint8_t u8;
 typedef int32_t i32;
 typedef size_t usize;
 typedef std::string str;
+typedef std::fstream fd;
 
 typedef struct {
 	unsigned char DIRECT_BLOCKS[3];    
@@ -18,7 +24,37 @@ typedef struct {
     unsigned char DOUBLE_INDIRECT_BLOCKS[3];
 } INODE_BLOCKS;
 
+INODE construct_inode(u8 is_used, u8 is_dir, str name, u8 size, INODE_BLOCKS blocks) {
+	INODE inode;
+	inode.IS_USED = is_used;
+	inode.IS_DIR = is_dir;
+
+	for(usize i = 0; i < 10; i++) {
+		inode.NAME[i] = i < name.size() ? name[i] : '\0';
+	}
+
+	inode.SIZE = size;
+	for (int i = 0; i < 3; i++) {
+		inode.DIRECT_BLOCKS[i] = blocks.DIRECT_BLOCKS[i];
+	}
+	for (int i = 0; i < 3; i++) {
+		inode.INDIRECT_BLOCKS[i] = blocks.INDIRECT_BLOCKS[i];
+	}
+	for (int i = 0; i < 3; i++) {
+		inode.DOUBLE_INDIRECT_BLOCKS[i] = blocks.DOUBLE_INDIRECT_BLOCKS[i];
+	}
+	return inode;
+}
+
+typedef struct {
+	char blockSize;
+	char numBlocks;
+	char numINodes;
+} MetaData;
+
 void truncate_file(str& fileName){ std::fstream file{fileName, std::ios::out | std::ios::trunc}; }
+
+c8 _getBitNFromByte(c8 byte, usize n){ return byte & (0x01 << n); } //(byte >> position) & 0x1
 
 usize _getBitMapSize(const c8 numBlocks)
 {
@@ -58,26 +94,33 @@ usize _blocksNeededToStore(usize content, c8 blockSize)
 	return std::ceil(static_cast<double>(content) / static_cast<double>(blockSize));
 }
 
-c8 _fetchBlockSize(std::fstream& fs)
+MetaData _fetchMetadata(fd& fs)
 {
-	c8 blockSize;
-	fs.seekg(_getBlockSizeOffSet())
-		.read(&blockSize, sizeof(c8));
-	return blockSize;
+	MetaData metaData{};
+	fs.seekg(_getMetaDataOffSet())
+		.read(reinterpret_cast<char*>(&metaData), sizeof(MetaData));
+	return metaData;
 }
 
-c8 _fetchNumBlocks(std::fstream& fs)
+usize _fetchBitMapSize(std::fstream& fs)
 {
-	c8 numBlocks;
-	fs.seekg(_getNumBlocksOffSet())
-		.read(&numBlocks, sizeof(c8));
-	return numBlocks;
+	return _getBitMapSize(_fetchMetadata(fs).numBlocks);
 }
 
-c8 _fetchBitMapSize(std::fstream& fs)
+usize _fetchBlocksOffset(fd& fs)
 {
-	return _getBitMapSize(_fetchNumBlocks(fs));
+	auto metaData = _fetchMetadata(fs);
+	return _getBlocksOffSet(metaData.numBlocks, metaData.numINodes);
 }
+
+c8 _fetchBitMapByte(fd& fs, usize byteIndex)
+{
+	c8 buff{};
+	fs.seekg(_getBitMapOffSet() + byteIndex*sizeof(c8))
+		.read(&buff, sizeof(c8));
+	return buff;
+}
+
 
 c8* _iNodeToWritable(INODE& inode) { return reinterpret_cast<c8*>(&inode);}
 
@@ -94,6 +137,35 @@ void _writeMetaData(
 		.write(&numInodes, sizeof(c8));
 }
 
+typedef struct {
+	str name;
+	bool isDir;
+} __FilePathStruct;
+typedef std::vector<__FilePathStruct> ParsedFilePath;
+
+ParsedFilePath _parsePath(str& path)
+{
+	auto parsedPath = ParsedFilePath();
+	parsedPath.push_back({ "/", true });
+
+	std::string charBuff{};
+	for (size_t i = 1 /* We already have the root */; i < path.size(); i++) {
+		if (path[i] == '/') {
+			parsedPath.push_back({ charBuff, true });
+			charBuff.clear();
+			continue;
+		}
+		if (i + 1 == path.size()) {
+			parsedPath.push_back({ charBuff, false });
+			break;
+		}
+
+		charBuff.push_back(path[i]);
+	}
+	
+	return parsedPath;
+}
+
 void _writeBitMap(
 	std::fstream& fs,
 	const c8 numBlocks,
@@ -107,15 +179,20 @@ void _writeBitMap(
 	}
 }
 
-// TODO: This io currently overwriting the whole byte
 void _writeBitMapAt(
 	std::fstream& fs,
 	const usize position,
-	const c8 value,
+	const bool value,
 	const usize byteOffset
 ){
-	fs.seekp(byteOffset + position);
-	fs.write(&value, sizeof(c8));
+	c8 buff{};
+	auto byteIndex = position / 8;
+	auto bitMapOffset = _getBitMapOffSet() + byteIndex;
+	fs.seekg(bitMapOffset)
+		.read(&buff, sizeof(c8));
+	buff |= static_cast<c8>(value) << (position % 8);
+	fs.seekp(bitMapOffset)
+		.write(&buff, sizeof(c8));
 }
 
 void _writeBitMapFill(
@@ -185,26 +262,85 @@ void _writeBlocksFill(
 	}
 }
 
-c8 _findEmptyBlockIndex(std::fstream& fs)
+usize _findEmptyBlockIndex(std::fstream& fs)
 {
-	std::vector<c8> bitMap{};
-	for(usize i = 0; i < _fetchBitMapSize(fs); i++){
-
+	for(usize byteIndex = 0; byteIndex < _fetchBitMapSize(fs); byteIndex++){
+		c8 bitMapByte{_fetchBitMapByte(fs, byteIndex)};
+		// We now check each bit
+		for(usize bitIndex = 0; bitIndex < 8; bitIndex++){
+			c8 bit{_getBitNFromByte(bitMapByte, bitIndex)};
+			if(bit == 0){
+				return _fetchBlocksOffset(fs) + (byteIndex*8 + bitIndex)*_fetchMetadata(fs).blockSize;
+			}
+		}
 	}
+	throw std::runtime_error("No free blocks");
 }
 
 INODE_BLOCKS _writeBlocks(std::fstream& fs, std::fstream& file, str& fileContent)
 {
-	c8 blockSize{};
-	fs.seekg(_getBlockSizeOffSet())
-		.read(&blockSize, sizeof(c8));
+	auto metaData = _fetchMetadata(fs);
 	
-	c8 blocksNeeded{_blocksNeededToStore(fileContent.size(), blockSize)};
+	auto blocksNeeded{_blocksNeededToStore(fileContent.size(), metaData.blockSize)};
 	
+	INODE_BLOCKS blocks{};
+
 	for(usize i = 0; i < blocksNeeded; i++){
-		fs.seekp(_findEmptyBlockIndex(fs))
-			.write(&(fileContent.c_str()[i*blockSize]), blockSize);
+		auto blockIndex = _findEmptyBlockIndex(fs);
+		// This should work, but for some reason it doesn't ¯\_(ツ)_/¯
+		/*auto blockIndex = _findEmptyBlockIndex(fs);
+		auto strBlock = fileContent.substr(i*metaData.blockSize, metaData.blockSize).c_str();
+		fs.seekp(blockIndex)
+			.write(strBlock, metaData.blockSize)
+			.flush();*/
+		// this only works for block size of 2
+		fs.seekp(blockIndex)
+			.write(&fileContent[i*metaData.blockSize], sizeof(c8))
+			.write(&fileContent[i*metaData.blockSize+1], sizeof(c8));
+		_writeBitMapAt(
+			fs,
+			(blockIndex - _getBlocksOffSet(metaData.numBlocks, metaData.numINodes)) / metaData.blockSize,
+			0x01,
+			_getBitMapOffSet()
+		);
+		blocks.DIRECT_BLOCKS[i] = blockIndex;
 	}
+
+	return blocks;
+}
+
+// @returns the index of the new inode
+usize _writeINode(fd& fs, INODE inode)
+{
+	auto metaData = _fetchMetadata(fs);
+	for(usize i = 0; i < static_cast<usize>(_fetchMetadata(fs).numINodes); i++){
+		INODE inodeBuff{};
+		fs.seekg(_getINodesOffSet(metaData.numBlocks) + i*sizeof(INODE))
+			.read(_iNodeToWritable(inodeBuff), sizeof(INODE));
+		if(inodeBuff.IS_USED == 0){
+			fs.seekp(_getINodesOffSet(metaData.numBlocks) + i*sizeof(INODE))
+				.write(_iNodeToWritable(inode), sizeof(INODE));
+			return _getINodesOffSet(metaData.numBlocks) + i*sizeof(INODE);
+		}
+	}
+}
+
+void _writeUpdateParent(fd& fs, ParsedFilePath path, usize inodeIndex)
+{
+	auto metaData = _fetchMetadata(fs);
+	INODE inodeBuff{};
+	for (int i = path.size() - 1; i >= 0; i--)
+	{
+		fs.seekg(_getINodesOffSet(metaData.numBlocks) + i*sizeof(INODE))
+			.read(_iNodeToWritable(inodeBuff), sizeof(INODE));
+		if(inodeBuff.IS_USED == 1){
+			if(inodeBuff.NAME == path[i]){
+				// We found the parent
+				//TODO: write to the blocks
+			}
+		}
+	}
+	
 }
 
 void initFs(std::string fsFileName, int blockSize, int numBlocks, int numInodes)
@@ -223,16 +359,19 @@ void addFile(std::string fsFileName, std::string filePath, std::string fileConte
 	std::fstream fs{ fsFileName, std::ios::binary | std::ios::in | std::ios::out };
 	std::fstream file{ filePath, std::ios::binary | std::ios::in | std::ios::out };
 	auto blocksIndex = _writeBlocks(fs, file, fileContent);
-	/*auto [parent, name] = parseFileName(filePath);
-	_writeInode(fs, parent, INODE{
-		0x01, // IS_USED
-		0x00, // IS_DIR
-		name, // NAME
-		static_cast<c8>(fileContent.size()), // SIZE
-		blocksIndex.directBlocks, // DIRECT_BLOCKS
-		blocksIndex.indirectBlocks, // INDIRECT_BLOCKS
-		blocksIndex.doubleIndirectBlocks // DOUBLE_INDIRECT_BLOCKS
-	});*/
+	
+	auto fileStructure = _parsePath(filePath);
+
+	auto inodeIndex = _writeINode(fs, construct_inode(
+		1, // IS_USED
+		0, // IS_DIR
+		fileStructure.back().name, // NAME
+		fileContent.size(), // SIZE
+		blocksIndex
+	));
+
+	_writeUpdateParent(fs, fileStructure, inodeIndex);
+	// Update parent directory size and add this node to the children list
 }
 
 void addDir(std::string fsFileName, std::string dirPath)

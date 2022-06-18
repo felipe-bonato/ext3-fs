c33ca63f1f5873ea033ce6c81c1fecac27dffcb6
Felipe Bonato <felipe13.bonato@gmail.com>
2022-06-18 12:45:07 -0300

    Fixed tests bug

--- main.cpp
+++ main.cpp
@@ -76,7 +76,7 @@ TEST(FsTest, case9){
 TEST(FsTest, case10){
     duplicate("fs-case10.bin", "fs-case10.bin.solucao");
     
-    move("fs-case10.bin", "/teste.txt", "/dec7556/teste.txt");
+    move("fs-case10.bin.solucao", "/teste.txt", "/dec7556/teste.txt");
     ASSERT_EQ(printSha256("fs-case10.bin.solucao"),std::string("36:EB:18:B6:6F:9C:1E:20:B1:3A:86:81:A7:9D:0B:2E:A4:B8:A1:8E:92:B1:FB:B3:70:15:E8:9E:48:47:FC:53"));
 }
 
@@ -90,7 +90,7 @@ TEST(FsTest, case11){
 TEST(FsTest, case12){
     duplicate("fs-case12.bin", "fs-case12.bin.solucao");
     
-    move("fs-case12.solucao", "/b.txt", "/a.txt");
+    move("fs-case12.bin.solucao", "/b.txt", "/a.txt");
     ASSERT_EQ(printSha256("fs-case12.solucao"),std::string("BC:2B:05:C8:8B:DF:02:41:3B:E3:86:8E:4C:CC:C1:FF:63:87:F9:A5:24:15:16:49:83:88:F0:75:18:D1:1B:BE"));
 }
 
